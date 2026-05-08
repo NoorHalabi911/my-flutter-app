607250249526649280338badddaae8f55a48d2c0
Noor Halabi <halabinoor18@gmail.com>
2026-05-08 13:13:46 +0300

    Remove unused signup field exports

--- lib/widgets/signup_widgets.dart
+++ lib/widgets/signup_widgets.dart
@@ -1,5 +1,3 @@
 export 'signup_confirm_password_field.dart';
-export 'signup_email_field.dart';
 export 'signup_google_button.dart';
 export 'signup_login_button.dart';
-export 'signup_password_field.dart';
